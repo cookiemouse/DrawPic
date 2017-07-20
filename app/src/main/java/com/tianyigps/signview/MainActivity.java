0f23e7992e964240e266fd498bc393339dba13bf
cookiemouse <cookiemouse@163.com>
2017-07-20 15:54:13 +0800

    实现签字控件SignView

---
+++ app/src/main/java/com/tianyigps/signview/MainActivity.java
@@ -0,0 +1,71 @@
+package com.tianyigps.signview;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.tianyigps.signviewlibrary.SignView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButtonSave, mButtonClear;
+    private SignView mSignView;
+
+    private String path = Environment.getExternalStorageDirectory().getPath() + "/test.png";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mButtonSave = (Button) findViewById(R.id.btn_activity_save);
+        mButtonClear = (Button) findViewById(R.id.btn_activity_clear);
+        mSignView = (SignView) findViewById(R.id.sv_activity);
+
+
+        File file = new File(path);
+        if (!file.exists()) {
+            throw new NullPointerException("file not exist");
+        }
+        Bitmap bitmap = BitmapFactory.decodeFile(path);
+        mSignView.setBitmap(bitmap);
+
+        mButtonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bitmap bitmap = mSignView.getPic();
+                File file = new File(path);
+                if (file.exists()) {
+                    file.delete();
+                }
+                try {
+                    FileOutputStream fileOutputStream = new FileOutputStream(file);
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, fileOutputStream);
+                    fileOutputStream.flush();
+
+                    fileOutputStream.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        mButtonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mSignView.clearPath();
+            }
+        });
+    }
+}
---
+++ signviewlibrary/src/main/java/com/tianyigps/signviewlibrary/SignView.java
@@ -0,0 +1,100 @@
+package com.tianyigps.signviewlibrary;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by cookiemouse on 2017/7/20.
+ */
+
+public class SignView extends View {
+
+    private static final String TAG = "SignView";
+
+    private Paint mPaint;
+    private Path mPath;
+
+    private Bitmap mBitmap;
+
+    public SignView(Context context) {
+        this(context, null);
+    }
+
+    public SignView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public SignView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);  //抗锯齿
+        mPaint.setDither(true);     //图像抖动处理
+        //  mPaint.setMaskFilter();     //paint边缘处理
+        mPaint.setColor(0xff222222);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(5);
+
+        mPath = new Path();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if (null != mBitmap) {
+            canvas.drawBitmap(mBitmap, 0, 0, mPaint);
+        }
+
+        canvas.drawPath(mPath, mPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN: {
+                mPath.moveTo(event.getX(), event.getY());
+                break;
+            }
+            case MotionEvent.ACTION_MOVE: {
+                mPath.lineTo(event.getX(), event.getY());
+                postInvalidate();
+                break;
+            }
+            default: {
+                Log.i(TAG, "onTouchEvent: default");
+            }
+        }
+//        return super.onTouchEvent(event);
+        return true;
+    }
+
+    //  清除图案
+    public void clearPath() {
+        mPath.reset();
+        mBitmap = null;
+        postInvalidate();
+    }
+
+    //  添加图象
+    public void setBitmap(Bitmap bitmap) {
+        this.mBitmap = bitmap;
+        postInvalidate();
+    }
+
+    //  保存图象
+    public Bitmap getPic() {
+        Bitmap bitmap = Bitmap.createBitmap(this.getWidth(), this.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        this.draw(canvas);
+        return bitmap;
+    }
+}
